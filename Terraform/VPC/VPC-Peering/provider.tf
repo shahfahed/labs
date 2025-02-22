60903dab985fa2e7f72f1ede5b6ea5833cfad751
Shah Fahad <shahfahed88@gmail.com>
2025-02-22 08:08:00 +0530

    removed secrets

--- Terraform/VPC/VPC-Peering/provider.tf
+++ Terraform/VPC/VPC-Peering/provider.tf
@@ -1,13 +1,13 @@
 provider aws {
-    access_key = "AKIA47CR3IEQTKHN3BMM"
-    secret_key = "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"
+    access_key = "*****"
+    secret_key = "*****"
     region = "us-east-1"
     alias = "nv-region"
 }
 
 provider aws {
-    access_key = "AKIA47CR3IEQTKHN3BMM"
-    secret_key = "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"
+    access_key = "*****"
+    secret_key = "*****"
     region = "us-east-2"
     alias = "oh-region"
 }
--- Terraform/VPC/custom-vpc.tf
+++ Terraform/VPC/custom-vpc.tf
@@ -1,6 +1,6 @@
 provider aws {
-    access_key = "AKIA47CR3IEQTKHN3BMM"
-    secret_key = "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"
+    access_key = "*****"
+    secret_key = "*****"
     region = "us-east-2"
 }
 
