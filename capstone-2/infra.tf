2b78b2874c2349be282fce239f151e90962ef7d7
Shah <shahfahed88@gmail.com>
2025-04-11 21:01:04 +0530

    updated infra.tf

--- capstone-2/infra.tf
+++ capstone-2/infra.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "cust-vpc" {
 resource "aws_subnet" "pub-sub" {
     vpc_id = aws_vpc.cust-vpc.id
     cidr_block = "10.0.0.0/28"
-    availability_zone = "us-east-2a"
+    availability_zone = "us-east-1a"
     map_public_ip_on_launch = true
     tags = {
         Name = "public-sub"
@@ -84,6 +84,13 @@ resource "aws_security_group" "pub-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
@@ -94,14 +101,28 @@ resource "aws_security_group" "pub-sg" {
 
 ## instance
 
+resource "aws_instance" "j-master" {
+    tags = {
+        Name = "j-master"
+    }
+    ami = "ami-"
+    instance_type = "t2.micro"
+    key_name = "nv-key"
+    subnet_id = aws_subnet.pub-sub.id
+    vpc_security_group_ids = [aws_security_group.pub-sg.id]
+    associate_public_ip_address = true
+    count = 1
+}
+
 resource "aws_instance" "j-worker" {
     tags = {
         Name = "j-worker"
     }
     ami = "ami-"
-    key = "nv-key"
+    instance_type = "t2.micro"
+    key_name = "nv-key"
     subnet_id = aws_subnet.pub-sub.id
-    security_groups = [aws_security_group.pub-sg.id]
+    vpc_security_group_ids = [aws_security_group.pub-sg.id]
     associate_public_ip_address = true
     count = 1
 }
@@ -114,7 +135,7 @@ resource "aws_instance" "k8s-master" {
     instance_type = "t2.micro"
     key_name = "nv-key"
     subnet_id = aws_subnet.pub-sub.id
-    security_groups = [aws_security_group.pub-sg.id]
+    vpc_security_group_ids = [aws_security_group.pub-sg.id]
     associate_public_ip_address = true
     count = 1
 }
@@ -124,9 +145,10 @@ resource "aws_instance" "k8s-worker" {
         Name = "k8s-worker-${(count.index)+1}"
     }
     ami = "ami-"
-    key = "nv-key"
+    instance_type = "t2.micro"
+    key_name = "nv-key"
     subnet_id = aws_subnet.pub-sub.id
-    security_groups = [aws_security_group.pub-sg.id]
+    vpc_security_group_ids = [aws_security_group.pub-sg.id]
     associate_public_ip_address = true
     count = 2
 }
